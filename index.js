df9c6b6b0da630b796965f50b48e94f11cd98cde
website-e <eakunyotub@gmail.com>
2025-05-30 18:23:24 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold">Selamat datang di ReaxzyPay!</h1>
+    </div>
+  )
+}
